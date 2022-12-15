7bacb66fad96963b13692e173dc3c07badc97295
Cody McCarney <cody@mccarney.com.au>
2022-12-15 13:31:10 +1100

    Still missing opensea nfts

--- stlTest/main.js
+++ stlTest/main.js
@@ -1,6 +1,4 @@
-
-
-const nfts = [
+const dogs = [
     { 
         image: "https://random.dog/510a4438-2cf4-49ea-8f0f-ef0ad11999e5.jpg", 
         title: "Dog"
@@ -105,230 +103,233 @@ const nfts = [
     
 ];
 
-let camera, scene, renderer, controls, composer;
-var hblur, vblur;
-let targets = {simple: [], sphere: []};
+if (!nfts[0]) {
+    nfts = dogs
+    // add option to put dogs instead of nfts
+    // let dogElement = document.createElement('div');
+    // dogElement.className = 'dog-element';
 
-init();
-animate();
+    // let writing = document.createElement('h2');
+    // writing.className = 'dog-h2'
+    // writing.innerHTML = "If you don't have any nfts, here are some dogs"
+    // dogElement.appendChild(writing)
 
-function init() {
-
-    initCamera();
-
-    initScene();
-
-    initObjects();
-
-    // addClickListeners();
-
-    initRenderer();
-
-    initTrackbarControls();
-
-    transform(targets.sphere, 2000);
-
-    window.addEventListener('resize', onWindowResize, false);
-
+    // let dogButton = document.createElement('button');
+    // dogButton.className = 'dog-button'
+    // dogButton.innerHTML = 'Click to Dismiss'
+    // dogElement.appendChild(dogButton)
 }
 
-function initCamera() {
 
-    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+var camera, scene, renderer;
+var controls;
+
+var objects = [];
+var targets = { sphere: [] };
+
+if (nfts) {
+    initiate();
+    animate();
+}
+
+function initiate() {
+
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 3000;
-
-}
-
-function initScene() {
 
     scene = new THREE.Scene();
 
-}
+    // sphere
 
-function initRenderer() {
+    var vector = new THREE.Vector3();
 
-    renderer = new THREE.CSS3DRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('container').appendChild(renderer.domElement);
-}
+    for ( var i = 0, l = nfts.length; i < l; i ++ ) {
 
-function initObjects() {
+        // create div for each nft object
+        let element = document.createElement('div');
+        element.className = 'element';
+        
+        let link = document.createElement('a');
+        // link.href = nfts[i].image;
+        element.appendChild(link);
+    
+        let symbol = document.createElement('img');
+        symbol.className = 'nft-image';
+        symbol.src = nfts[i].image;
+        link.appendChild(symbol);
 
-    simpleObjectsLayout();
-    generateGeometricLayouts();
+        let title = document.createElement('h2');
+        title.className = "title";
+        title.innerHTML = nfts[i].title      
+        element.appendChild(title)
 
-}
-
-// function addClickListeners() {
-//     addClickListener(targets.sphere, 'sphere');
-// }
-
-function simpleObjectsLayout() {
-
-    for (let i = 0; i < nfts.length; i++) {
-
-        let object = new THREE.CSS3DObject(htmlElement(nfts, i));
+        var object = new THREE.CSS3DObject( element );
         object.position.x = Math.random() * 4000 - 2000;
         object.position.y = Math.random() * 4000 - 2000;
         object.position.z = Math.random() * 4000 - 2000;
+        scene.add( object );
 
-        scene.add(object);
-        targets.simple.push(object);
+        objects.push( object );
+        let iNum = i
+        element.addEventListener('click', ()=>elementClickHandler(iNum), false);
+
 
     }
 
+    var vector = new THREE.Vector3();
+
+    // sphere 
+
+    for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+        var phi = Math.acos( -1 + ( 2 * i ) / l );
+        var theta = Math.sqrt( l * Math.PI ) * phi;
+
+        var object = new THREE.Object3D();
+        let size = 800
+
+        object.position.x = size * Math.cos( theta ) * Math.sin( phi );
+        object.position.y = size * Math.sin( theta ) * Math.sin( phi );
+        object.position.z = size * Math.cos( phi );
+
+        vector.copy( object.position ).multiplyScalar( 2 );
+        object.lookAt( vector );
+
+        targets.sphere.push( object );
+
+    }
+
+
+    //
+
+    renderer = new THREE.CSS3DRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.domElement.style.position = 'absolute';
+    document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+    //
+
+    controls = new THREE.TrackballControls( camera, renderer.domElement );
+    controls.rotateSpeed = 3;
+    controls.minDistance = 500;
+    controls.maxDistance = 6000;
+    controls.addEventListener( 'change', render );
+
+
+    transform( targets.sphere, 2000 );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
-function htmlElement(nfts, i) {
-    let element = document.createElement('div');
-    element.className = 'nft';
-    // element.style.backgroundColor = 'rgba(0,127,127,' + (Math.random() * 0.5 + 0.25) + ')';
 
-    let link = document.createElement('a');
-    link.href = nfts[i].image;
+function transform( targets, duration ) {
+    TWEEN.removeAll();
 
-    element.appendChild(link);
+    for ( var i = 0; i < objects.length; i ++ ) {
 
+        var object = objects[ i ];
+        var target = targets[ i ];
 
-    let symbol = document.createElement('img');
-    symbol.className = 'nft-image';
-    symbol.src = nfts[i].image;
-    symbol.style.width = "300px";
-    link.appendChild(symbol);
+        new TWEEN.Tween( object.position )
+            .to( { x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration )
+            .easing( TWEEN.Easing.Exponential.InOut )
+            .start();
 
+        new TWEEN.Tween( object.rotation )
+            .to( { x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration )
+            .easing( TWEEN.Easing.Exponential.InOut )
+            .start();
 
-    element.addEventListener('click', ()=>elementClickHandler(i), false);
+    }
 
-    return element;
+    new TWEEN.Tween( this )
+        .to( {}, duration * 2 )
+        .onUpdate( render )
+        .start();
+
 }
 
 function elementClickHandler(i){
+    transform(targets.sphere, 1000)
+    // move thing towards perspective, this too time consuming for right now, but could be cool
+    // let camX, camY, camZ = 0
+    // if (camera.position.x > 0) {
+    //     camX = camera.position.x - 300
+    // } else if (camera.position.x <= 300 || camera.position ) {
+    //     camX = 0
+    // } else {
+    //     camX = camera.position.x + 300
+    // }
 
-    transform(targets.sphere, 500);
-
-    new TWEEN.Tween(targets.simple[i / 5].position)
+    // add a click listener for when you want to access the opensea link
+    new TWEEN.Tween(camera.position)
         .to({
             x: 0,
             y: 0,
-            z: 2500
+            z: 3000
+        }, Math.random() * 2000 + 2000)
+        .easing( TWEEN.Easing.Exponential.InOut )
+        .start();
+
+
+
+    new TWEEN.Tween(objects[i].position)
+        .to({
+            x: 0,
+            y: 0,
+            z: 2200
         }, Math.random() * 2000 + 2000)
         .easing(TWEEN.Easing.Exponential.InOut)
         .start();
+    
+    new TWEEN.Tween(objects[i].rotation)
+        .to({
+            x: 0,
+            y: 0,
+            z: camera.rotation.x
+        }, Math.random() * 2000 + 2000)
+        .easing( TWEEN.Easing.Exponential.InOut )
+        .start();    
+
+
+        console.log(camera.rotation, 1)
+        console.log(objects[i].rotation, 2)
 
     new TWEEN.Tween(this)
         .to({}, 2000 * 2)
         .onUpdate(render)
         .start();
-}
 
-
-function addClickListener(target, elementId) {
-
-    const button = document.getElementById(elementId);
-
-    button.addEventListener('click', function () {
-        transform(target, 2000);
-    }, false);
-
-}
-
-function initTrackbarControls() {
-    controls = new THREE.TrackballControls(camera, renderer.domElement);
-    controls.rotateSpeed = 2;
-    controls.minDistance = 500;
-    controls.maxDistance = 6000;
-    controls.addEventListener('change', render);
-}
-
-function generateGeometricLayouts() {
-
-    let sphereVector = new THREE.Vector3();
-    let helixVector = new THREE.Vector3();
-
-    for (let i = 0, l = targets.simple.length; i < l; i++) {
-        addSphereObject(sphereVector, i, l);
-    }
-
-}
-
-function addSphereObject(sphereVector, index, length) {
-
-    const phi = Math.acos(-1 + (2 * index) / length);
-    const theta = Math.sqrt(length * Math.PI) * phi;
-    let object = new THREE.Object3D();
-
-    object.position.setFromSphericalCoords(800, phi, theta);
-
-    sphereVector.copy(object.position).multiplyScalar(2);
-
-    object.lookAt(sphereVector);
-
-    targets.sphere.push(object);
-}
-
-function transform(target, duration) {
-
-    TWEEN.removeAll();
-
-    for (let i = 0; i < targets.simple.length; i++) {
-        let object = targets.simple[i];
-        let targetObject = target[i];
-        transformObjectPosition(object, targetObject, duration);
-        transformObjectRotation(object, targetObject, duration);
-    }
-
-    new TWEEN.Tween(this)
-        .to({}, duration * 2)
-        .onUpdate(render)
-        .start();
-
-}
-
-function transformObjectPosition(object, targetObject, duration) {
-
-    new TWEEN.Tween(object.position)
-        .to({
-            x: targetObject.position.x,
-            y: targetObject.position.y,
-            z: targetObject.position.z
-        }, Math.random() * duration + duration)
-        .easing(TWEEN.Easing.Exponential.InOut)
-        .start();
-
-}
-
-function transformObjectRotation(object, targetObject, duration) {
-
-    new TWEEN.Tween(object.rotation)
-        .to({
-            x: targetObject.rotation.x,
-            y: targetObject.rotation.y,
-            z: targetObject.rotation.z
-        }, Math.random() * duration + duration)
-        .easing(TWEEN.Easing.Exponential.InOut)
-        .start();
-
+    console.log(camera.rotation, 1)
+    console.log(objects[i].rotation, 2)
 }
 
 function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
     render();
-
-}
-
-function render() {
-
-    renderer.render(scene, camera);
 
 }
 
 function animate() {
 
-    requestAnimationFrame(animate);
+    requestAnimationFrame( animate );
+
     TWEEN.update();
+
     controls.update();
-    // composer.render();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
 }
